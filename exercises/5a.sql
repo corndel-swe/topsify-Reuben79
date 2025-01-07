b598761ab6fffaeca9a5d379e4c7f3b69537401b
reuben longley <reuben.longley@clarionhg.com>
2025-01-07 14:08:53 +0000

    E5&6

--- exercises/5a.sql
+++ exercises/5a.sql
@@ -1,1 +1,13 @@
 -- How many tracks are longer than 5 minutes?
+-- tracks.duration_ms
+-- what measurement is duration_ms. millisecs?? (need to convert to minutes)
+
+-- SELECT COUNT(id)
+-- FROM tracks
+
+--5mins = 8000 millisecs
+
+SELECT COUNT (duration_ms)
+FROM tracks
+WHERE tracks.duration_ms > 300000
+;
--- exercises/6a.sql
+++ exercises/6a.sql
@@ -2,3 +2,8 @@
 -- Include a count of how many albums they have released
 -- Order your results by artist name in ascending order
 -- (You should group the artists by id, not by name)
+SELECT artists.name, COUNT(albums.id) 
+FROM artists
+INNER JOIN albums ON albums.artist_id = artists.id
+GROUP BY artists.id, artists.name
+ORDER BY artists.name ASC;
